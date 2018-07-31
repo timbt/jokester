68b8a98918ce30ffb5cbfa9ca1ed8d6562e6c58e
Timothy Beattie <timothy.beattie@canada.ca>
2018-07-31 19:44:56 +0000

    remove duplicate files

--- src/JokeButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export default function JokeButton(props) {
-    const buttonMessage = props.welcome ? 
-        'Tell me a joke!' : 
-        'Tell me another joke!';
-    return <button onClick={props.onClick}>{buttonMessage}</button>
-}
--- src/Welcome.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function Welcome () {
-    return <p>Welcome to Jokester!</p>
-}
